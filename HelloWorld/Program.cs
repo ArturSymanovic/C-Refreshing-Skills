754678a3060a30c4d565d06ef611ae79de78a6bc
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-13 23:34:52 +0100

    Promitive Types and Console

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,28 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Artur";
+            bool isWorking = true;
+
+            var random = 123;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(random);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            
+            
         }
     }
 }
