61808ede1d68d5af3243ea948a3e8a6193aa0847
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-14 19:32:20 +0100

    Moving SummarizeText() to new StringUtility class

--- CSharpRefreshment/Program.cs
+++ CSharpRefreshment/Program.cs
@@ -8,27 +8,11 @@ namespace CSharpRefreshment
     {
         static void Main(string[] args)
         {
-
             var sentence = "This is going to be very very very very very very long text";
-            const int MaxLength = 20;
-            if (sentence.Length < MaxLength)
-                Console.WriteLine(sentence);
-            else
-            {
-                sentence.Substring(0, MaxLength);
-                var words = sentence.Split(' ');
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
-                foreach (var word in words)
-                {
-                    summaryWords.Add(word);
-                    totalCharacters += word.Length + 1;
-                    if (totalCharacters > MaxLength)
-                        break;
-                }
-                var summary = String.Join(" ", summaryWords) + "...";
-                Console.WriteLine(summary);
-            }
+            Console.WriteLine(StringUtility.SummarizeText(sentence, 25));
+
         }
+
+        
     }
 }
