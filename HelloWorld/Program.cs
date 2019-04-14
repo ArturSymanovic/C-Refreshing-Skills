f17523908e431fe98397ec0c00ab2140ca9f2688
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-14 11:00:07 +0100

    Array Initialisation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,15 +7,25 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
+            var numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
 
-            john.Introduce();
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            string[] names = new string[3] { "Jack", "John", "Marry" };
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }
