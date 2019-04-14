1839ce0b81f6bb03afae447e497bc5559f877bff
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-14 12:06:35 +0100

    Reference and Value Types

--- HelloWorld/Person.cs
+++ HelloWorld/Person.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         public string FirstName;
         public string LastName;
-
+        public int Age;
         public void Introduce()
         {
             Console.WriteLine("My name is " + FirstName + " " + LastName);
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -13,19 +13,37 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var method = ShippingMethod.Express;
-            Console.WriteLine(method);
+            //Value Types
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(String.Format("a: {0}, b: {1}", a, b));
 
-            Console.WriteLine((int)method);
-            var methodId = 2;
-            Console.WriteLine((ShippingMethod)methodId);
+            //Reference Types
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(String.Format("array1[0]: {0}, array2[0]: {1}"
+                                , array1[0]
+                                , array2[0]));
 
-            //Writeline implicitly calls ToString() anyways
-            Console.WriteLine(method.ToString());
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
 
-            var methodName = "Express";
-            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-            Console.WriteLine("aa " + shippingMethod);
+            Person person = new Person { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }
