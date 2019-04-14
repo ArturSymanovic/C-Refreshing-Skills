9e2c6ee6f5f0a4de2af1cc24730966f0f3f9e94d
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-14 19:43:14 +0100

    String Builder

--- CSharpRefreshment/Program.cs
+++ CSharpRefreshment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using HelloWorld.Math;
 
 namespace CSharpRefreshment
@@ -8,8 +9,28 @@ namespace CSharpRefreshment
     {
         static void Main(string[] args)
         {
-            var sentence = "This is going to be very very very very very very long text";
-            Console.WriteLine(StringUtility.SummarizeText(sentence, 25));
+            var builder = new StringBuilder();
+            //builder.Append('-', 10);
+            //builder.AppendLine();
+            //builder.Append("Header");
+            //builder.AppendLine();
+            //builder.Append('-', 10);
+            //builder.Replace('-', '+');
+            //builder.Remove(0, 10);
+            //builder.Insert(0, new string('-', 10));
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine("First Char: " + builder[0]);
+            Console.WriteLine(builder);
 
         }
 
