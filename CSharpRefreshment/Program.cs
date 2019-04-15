619f79c80363538cd1cbc3ba4f38bab5275bb285
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-15 20:24:49 +0100

    Path class

--- CSharpRefreshment/Program.cs
+++ CSharpRefreshment/Program.cs
@@ -10,43 +10,11 @@ namespace CSharpRefreshment
     {
         static void Main(string[] args)
         {
-            var path = @"c:\somefile.jpg";
-            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
-            File.Delete(path);
-            if (File.Exists(path))
-            {
-                //
-            }
-            var content = File.ReadAllText(path);
-
-            var fileInfo = new FileInfo(path);
-            fileInfo.CopyTo("...");
-            fileInfo.Delete();
-            if (fileInfo.Exists)
-            {
-                //
-            }
-            FileStream fStream1 = fileInfo.OpenRead();
-
-            Directory.CreateDirectory(@"c:\temp\folder1");
-            var files = Directory.GetFiles(@"c:\projects\charpfundamentals", "*.*", SearchOption.AllDirectories);
-            foreach (var item in files)
-            {
-                Console.WriteLine(item);
-            }
-
-            var directories = Directory.GetDirectories(@"c:\projects", "*,*", SearchOption.AllDirectories);
-            foreach (var item in directories)
-            {
-                Console.WriteLine(item);
-            }
-
-            Directory.Exists("...");
-            var directoryInfo = new DirectoryInfo("...");
-            directoryInfo.GetFiles();
-
-
-
+            var path = @"C:\Users\Artur Symanovic\test.txt";
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("FileName: " + Path.GetFileName(path));
+            Console.WriteLine("Filename without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
         }
     }
 }
