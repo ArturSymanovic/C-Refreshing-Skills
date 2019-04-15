6b3d7e5694aa1de8b7b70e53540db1e6ac12d55e
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-15 21:17:18 +0100

    Debugging Three smallest numbers program

--- CSharpRefreshment/Program.cs
+++ CSharpRefreshment/Program.cs
@@ -10,11 +10,42 @@ namespace CSharpRefreshment
     {
         static void Main(string[] args)
         {
-            var path = @"C:\Users\Artur Symanovic\test.txt";
-            Console.WriteLine("Extension: " + Path.GetExtension(path));
-            Console.WriteLine("FileName: " + Path.GetFileName(path));
-            Console.WriteLine("Filename without extension: " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+            var numbers = new List<int> { 1, 2 };
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        private static List<int> GetSmallests(List<int> list, int count)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list", "Initial List of numbers is null");
+            if (count > list.Count || count<=0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 0 and the number of integers in the initial list.");
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+            return smallests;
+        }
+
+        private static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is smallest
+            var min = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
         }
     }
 }
