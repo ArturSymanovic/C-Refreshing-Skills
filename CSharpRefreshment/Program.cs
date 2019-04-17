d8ebb752c49e36c150b593c13bb2db47b7bd685d
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-17 20:33:18 +0100

    Params, ref, our, constructor overloading

--- CSharpRefreshment/Program.cs
+++ CSharpRefreshment/Program.cs
@@ -10,12 +10,50 @@ namespace CSharpRefreshment
     {
         static void Main(string[] args)
         {
-            Customer customer = new Customer(1, "John");
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            //UsePoints();
+            //UseParams();
 
-            var order = new Order();
-            customer.Orders.Add(order);
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion Failed");
+            }
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion Failed.");
+        }
+
+        private static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+
+                point.Move(new Point(40, 60));
+                Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured...");
+            }
         }
     }
 }
