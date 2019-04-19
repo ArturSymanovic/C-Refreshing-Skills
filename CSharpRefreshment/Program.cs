b2e660a7147bd43ad456c57c6a747d1f14e5814a
Artur Symanovic <artur.symanovic@gmail.com>
2019-04-19 13:28:35 +0100

    Composition

--- CSharpRefreshment/Program.cs
+++ CSharpRefreshment/Program.cs
@@ -11,9 +11,14 @@ namespace CSharpRefreshment
     {
         static void Main(string[] args)
         {
-            var text = new Text();
-            text.Width = 100;
-            text.Copy();
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+
+            dbMigrator.Migrate();
+            installer.Install();
         }
     }
 }
